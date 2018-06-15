860cd66fb8a73b7a063616e23d9e8b21f85fee5b
Ftibw <ftibw@live.com>
2018-06-15 10:44:20 +0800

    yml自定义属性千万要注意空格问题无编译错误,但是运行时报错

--- harbor-web/src/main/java/com/whxm/harbor/HarborWebApplication.java
+++ harbor-web/src/main/java/com/whxm/harbor/HarborWebApplication.java
@@ -3,17 +3,21 @@ package com.whxm.harbor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @MapperScan("com.whxm.harbor.mapper")
+@PropertySource(value = {"classpath:config/application.yml"},ignoreResourceNotFound = true,encoding = "utf-8")
 public class HarborWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HarborWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HarborWebApplication.class, args);
+    }
 
-	/*@Override	extends SpringBootServletInitializer
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(HarborWebApplication.class);
-	}*/
+    /*@Override   extends SpringBootServletInitializer
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HarborWebApplication.class);
+    }*/
 }
