7b5f79df296646279f9543c31392ca674e800450
Ftibw <ftibw@live.com>
2018-06-15 13:26:06 +0800

    yml自定义属性千万要注意空格问题无编译错误,但是运行时报错

---
+++ harbor-common/src/main/java/com/whxm/harbor/constant/Constant.java
@@ -0,0 +1,14 @@
+package com.whxm.harbor.constant;
+
+public class Constant {
+
+    public static final Object DEFAULT_QUERY_CONDITION = null;
+
+    public static final Integer INCREMENT_ID_DEFAULT_VALUE = null;
+
+    public static final Integer RECORD_IS_DELETED = 0;
+
+    public static final Integer RECORD_NOT_DELETED = 1;
+
+    public static final Object NO_DATA = null;
+}
