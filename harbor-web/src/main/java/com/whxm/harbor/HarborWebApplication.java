1734743b2cf4abf83d2f7588c061b4ca074bb590
Ftibw <ftibw@live.com>
2018-06-20 21:38:45 +0800

    harbor update

--- harbor-web/src/main/java/com/whxm/harbor/HarborWebApplication.java
+++ harbor-web/src/main/java/com/whxm/harbor/HarborWebApplication.java
@@ -6,28 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.whxm.harbor.mapper")
-//@RestController
 public class HarborWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HarborWebApplication.class, args);
     }
 
-    /*@Override   extends SpringBootServletInitializer
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(HarborWebApplication.class);
-    }*/
-
-/*    @Autowired
-    private RedisDistributedLock lock;
-
-    @GetMapping("/info")
-    public String tryAcquire() {
-        return String.valueOf(lock.tryAcquire("1", "2", 100000));
-    }
-
-    @GetMapping("/info/")
-    public String tryRelease() {
-        return String.valueOf(lock.tryRelease("1", "2"));
-    }*/
 }
