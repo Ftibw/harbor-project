7fd498e967600cf1c1b6377432f929db4ff3d469
Ftibw <ftibw@live.com>
2018-06-21 14:15:43 +0800

    harbor update

--- harbor-web/src/main/java/com/whxm/harbor/controller/ScreensaverMaterialController.java
+++ harbor-web/src/main/java/com/whxm/harbor/controller/ScreensaverMaterialController.java
@@ -13,7 +13,6 @@ import com.whxm.harbor.utils.FileUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.hibernate.validator.constraints.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
