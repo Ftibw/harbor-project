415a6bfd53053aeae88e5cf4c90e142ea6d33877
Ftibw <ftibw@live.com>
2018-07-02 21:47:03 +0800

    simple file

--- harbor-domain/src/main/java/com/whxm/harbor/bean/User.java
+++ harbor-domain/src/main/java/com/whxm/harbor/bean/User.java
@@ -2,9 +2,7 @@ package com.whxm.harbor.bean;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.hibernate.validator.constraints.NotBlank;
-import org.hibernate.validator.constraints.NotEmpty;
 
-import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
 public class User {
