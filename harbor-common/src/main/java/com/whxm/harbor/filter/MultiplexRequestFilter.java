f0726b3438dc88e9b8bde0f358bcc258d976379d
Ftibw <ftibw@live.com>
2018-07-09 12:41:36 +0800

    update latest .

--- harbor-common/src/main/java/com/whxm/harbor/filter/MultiplexRequestFilter.java
+++ harbor-common/src/main/java/com/whxm/harbor/filter/MultiplexRequestFilter.java
@@ -5,7 +5,6 @@ import com.whxm.harbor.constant.Constant;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Objects;
 
