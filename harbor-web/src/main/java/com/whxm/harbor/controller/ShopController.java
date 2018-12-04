da056f210c642481c88989767e94195df0e7ccad
Ftibw <ftibw@live.com>
2018-12-04 22:58:27 +0800

    load cache on start

--- harbor-web/src/main/java/com/whxm/harbor/controller/ShopController.java
+++ harbor-web/src/main/java/com/whxm/harbor/controller/ShopController.java
@@ -18,7 +18,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
