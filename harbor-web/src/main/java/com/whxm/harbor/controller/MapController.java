a5b6d105e17a14cba2d3b1f0e21285c0c992249c
Ftibw <ftibw@live.com>
2018-12-05 17:02:04 +0800

    load cache on start

--- harbor-web/src/main/java/com/whxm/harbor/controller/MapController.java
+++ harbor-web/src/main/java/com/whxm/harbor/controller/MapController.java
@@ -21,7 +21,6 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
